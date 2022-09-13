56fd13e2a11769ca38a9cd780549a75e6392233d
Daisy Yi <daisyyi@rocketmail.com>
2022-09-13 13:20:49 -0700

    commit tree cd49ff1a35b7b5e29d180ea3aca9b3d02c87f930

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+newHeader.textContent = "Daisy is the champion";
+newHeader.id = 'victory';
